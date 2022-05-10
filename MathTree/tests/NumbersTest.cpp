045e3796a293a5e89cc2c664a20349cfc0dfe177
Michele Asara <asara.michele@gmail.com>
2022-05-11 00:20:14 +0100

    Added more parsing tests to NumbersTest. Also removed previous test fixture as most tests so far cover a static function.

--- MathTree/tests/NumbersTest.cpp
+++ MathTree/tests/NumbersTest.cpp
@@ -3,11 +3,35 @@
 
 using MathTree::RealNumber;
 
-class NumbersTest: public ::testing::Test {
-    protected:
+TEST(NumbersTest, EvaluatingReturnsTheNumberItself) {
     RealNumber number{42};
-};
-
-TEST_F(NumbersTest, EvaluatingReturnsTheNumberItself) {
     EXPECT_EQ(number.evaluate(), 42);
+}
+
+TEST(NumbersTest, ReturnsNullWhenParsingFails) {
+    EXPECT_EQ(RealNumber::parse(""), nullptr);
+}
+
+TEST(NumbersTest, CanParseAPositiveIntegerWithImplicitSign) {
+    auto parsed = RealNumber::parse("42");
+    ASSERT_NE(parsed, nullptr);
+    EXPECT_EQ(parsed->evaluate(), 42);
+}
+
+TEST(NumbersTest, CanParseAPositiveIntegerWithExplicitSign) {
+    auto parsed = RealNumber::parse("+42");
+    ASSERT_NE(parsed, nullptr);
+    EXPECT_EQ(parsed->evaluate(), 42);
+}
+
+TEST(NumbersTest, CanParseANegativeInteger) {
+    auto parsed = RealNumber::parse("-42");
+    ASSERT_NE(parsed, nullptr);
+    EXPECT_EQ(parsed->evaluate(), -42);
+}
+
+TEST(NumbersTest, CanParseFloatingPointNumbers) {
+    auto parsed = RealNumber::parse("42.78");
+    ASSERT_NE(parsed, nullptr);
+    EXPECT_EQ(parsed->evaluate(), 42.78);
 }
