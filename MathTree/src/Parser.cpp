c8250a67230d3c83f5b2b29507ee96caa882288a
Michele Asara <asara.michele@gmail.com>
2022-06-17 00:06:15 +0100

    Changed PrattParser's minimum priority. This affects parse() calls and the sentinel value of infixPriorityFor(Token).

--- MathTree/src/Parser.cpp
+++ MathTree/src/Parser.cpp
@@ -1,5 +1,6 @@
 #include "Parser.hpp"
 #include <stdexcept>
+#include <limits>
 
 namespace MathTree {
 
@@ -21,7 +22,7 @@ private:
 PrattParser::PrattParser(std::unique_ptr<Lexer> lexer): m_lexer(std::move(lexer)) {}
 
 std::unique_ptr<Expression> PrattParser::parse() {
-  return parse(0);
+  return parse(std::numeric_limits<int>::min());
 }
 
 std::unique_ptr<Expression> PrattParser::parse(int priority) {
@@ -58,7 +59,7 @@ Token PrattParser::consumeCurrentToken() {
 
 int PrattParser::infixPriorityFor(Token const& token) {
   if (m_infixParselets.count(token.type()) <= 0) {
-    return 0;
+    return std::numeric_limits<int>::min();
   }
   InfixParselet const& infix = *m_infixParselets[token.type()];
   return infix.priority();
