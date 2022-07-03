6c720c60339543879b65f532dbb1f0f1d22478d6
Michele Asara <asara.michele@gmail.com>
2022-07-03 18:25:08 +0100

    Added more tests for the prefix parselets.

--- MathTree/tests/PrefixParseletsTest.cpp
+++ MathTree/tests/PrefixParseletsTest.cpp
@@ -7,7 +7,9 @@
 #include "Token.hpp"
 
 using ::testing::ByMove;
+using ::testing::NotNull;
 using ::testing::Return;
+using ::testing::WhenDynamicCastTo;
 
 using MathTree::GroupParselet;
 using MathTree::LogarithmParselet;
@@ -21,6 +23,19 @@ protected:
   NiceAbstractPrattParserMock parserMock;
 };
 
+TEST_F(PrefixParseletsTest, groupParseletReturnsTheExpressionItObtainsFromTheParser) {
+  GroupParselet group;
+  std::unique_ptr<NiceExpressionMock> expressionMockPtr{std::make_unique<NiceExpressionMock>()};
+  auto& expressionMock = *expressionMockPtr;
+  ON_CALL(parserMock, consumeCurrentToken).WillByDefault(
+                                                 Return(Token{TokenType::ClosingBracket, ")"}));
+  EXPECT_CALL(parserMock, parse()).WillOnce(
+                                      Return(ByMove(std::move(expressionMockPtr))));
+
+  auto parseResult = group.parse(parserMock, Token{TokenType::OpeningBracket, "("});
+  EXPECT_EQ(parseResult.get(), &expressionMock);
+}
+
 TEST_F(PrefixParseletsTest, groupParseletThrowsIfTheTokenAfterIsNotAClosingBracket) {
   GroupParselet group;
   EXPECT_CALL(parserMock, consumeCurrentToken).WillOnce(
@@ -33,6 +48,13 @@ TEST_F(PrefixParseletsTest, groupParseletDoesNotThrowIfTheTokenAfterIsAClosingBr
   EXPECT_CALL(parserMock, consumeCurrentToken).WillOnce(
                                                    Return(Token{TokenType::ClosingBracket, ")"}));
   EXPECT_NO_THROW(group.parse(parserMock, Token{TokenType::OpeningBracket, "("}));
+}
+
+TEST_F(PrefixParseletsTest, squareRootParseletReturnsAnObjectOfTheCorrespondingExpressionType) {
+  SquareRootParselet squareRoot(1);
+  auto parseResult = squareRoot.parse(parserMock, Token{TokenType::SquareRoot, "sqrt"});
+  ASSERT_THAT(parseResult, NotNull());
+  EXPECT_THAT(parseResult.get(), WhenDynamicCastTo<MathTree::SquareRootExpression*>(NotNull()));
 }
 
 TEST_F(PrefixParseletsTest, squareRootParseletAsksForItsArgumentByParsingWithThePriorityUsedForItsConstruction) {
@@ -49,6 +71,13 @@ TEST_F(PrefixParseletsTest, negativeSignParseletAsksForItsArgumentByParsingWithT
   negation.parse(parserMock, Token{TokenType::Minus, "-"});
 }
 
+TEST_F(PrefixParseletsTest, negativeSignParseletReturnsAnObjectOfTheCorrespondingExpressionType) {
+  NegativeSignParselet parselet(1);
+  auto parseResult = parselet.parse(parserMock, Token{TokenType::Minus, "-"});
+  ASSERT_THAT(parseResult, NotNull());
+  EXPECT_THAT(parseResult.get(), WhenDynamicCastTo<MathTree::NegativeSignExpression*>(NotNull()));
+}
+
 TEST_F(PrefixParseletsTest, logarithmParseletAsksForItsArgumentByParsingWithThePriorityUsedForItsConstruction) {
   int priority = 1;
   LogarithmParselet logarithm(priority);
@@ -57,4 +86,11 @@ TEST_F(PrefixParseletsTest, logarithmParseletAsksForItsArgumentByParsingWithTheP
   logarithm.parse(parserMock, Token{TokenType::Log, "log"});
 }
 
+TEST_F(PrefixParseletsTest, logarithmSignParseletReturnsAnObjectOfTheCorrespondingExpressionType) {
+  LogarithmParselet parselet(1);
+  ON_CALL(parserMock, parse(1)).WillByDefault(
+                                        Return(ByMove(std::make_unique<NiceExpressionMock>())));
+  auto parseResult = parselet.parse(parserMock, Token{TokenType::Log, "log"});
+  ASSERT_THAT(parseResult, NotNull());
+  EXPECT_THAT(parseResult.get(), WhenDynamicCastTo<MathTree::LogarithmExpression*>(NotNull()));
 }
